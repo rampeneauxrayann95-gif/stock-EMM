351d82422436b1485cbc0727db8994944fca7bb8
rampeneauxrayann95-gif <rampeneauxrayann95@gmail.com>
2025-11-07 12:00:05 +0100

    Add files via upload

---
+++ database.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+from sqlmodel import SQLModel, create_engine, Session
+import os
+
+DB_URL = os.getenv("DATABASE_URL", "sqlite:///./stock.db")
+connect_args = {"check_same_thread": False} if DB_URL.startswith("sqlite") else {}
+
+engine = create_engine(DB_URL, echo=False, connect_args=connect_args)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+@contextmanager
+def get_session():
+    with Session(engine) as session:
+        yield session
